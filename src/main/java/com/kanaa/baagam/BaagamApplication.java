bc22b8f28085e72b5b0f116d79d1880dc93302f9
Preethi Vuchuru <PreethiVuchuru@preethivuchuru.attlocal.net>
2023-12-17 22:37:21 -0600

    Initial Setup

---
+++ src/main/java/com/kanaa/baagam/BaagamApplication.java
@@ -0,0 +1,13 @@
+package com.kanaa.baagam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaagamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaagamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanaa/baagam/BaagamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanaa.baagam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaagamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
